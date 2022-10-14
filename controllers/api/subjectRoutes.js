d3e5557c5162a6c46516b435d1a75f1b2f2570e8
Joon Lee <jlee@alphalab.com>
2022-10-13 19:22:05 -0700

    subject routes crud

--- controllers/api/subjectRoutes.js
+++ controllers/api/subjectRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
     }
   });
   
-  // GET a single subject
+// GET a single subject
   router.get('/:id', async (req, res) => {
     try {
       const driverData = await Subjects.findByPk(req.params.id, {
@@ -37,5 +37,51 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+// CREATE a new subject
+router.post('/', async (req, res) => {
+  try {
+    const subjectData = await Subjects.create(req.body);
+    res.status(200).json(subjectData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// UPDATE a subject
+router.put('/:id', async (req, res) => {
+  try {
+    const subjectData = await Subjects.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!subjectData[0]) {
+      res.status(404).json({ message: 'No subjects with this id!' });
+      return;
+    }
+    res.status(200).json(subjectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a subject
+router.delete('/:id', async (req, res) => {
+  try {
+    const subjectData = await Subjects.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!subjectData) {
+      res.status(404).json({ message: 'No subjects with this id!' });
+      return;
+    }
+    res.status(200).json(subjectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   
-  module.exports = router;
+module.exports = router;
