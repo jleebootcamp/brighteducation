b61f5814904c3804abca0c16d2a2b4dd85fdc5b6
Joon Lee <jlee@alphalab.com>
2022-10-13 18:28:55 -0700

    adjusted index api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const parentRoutes = require("./parentRoutes");
+const enrollmentRoutes = require('./enrollmentRoutes');
+const studentRoutes = require('./studentRoutes');
+const subjectRoutes = require('./subjectRoutes');
+const tutorRoutes = require('./tutorRoutes');
 
 router.use("/parents", parentRoutes);
+router.use("/enrollments", enrollmentRoutes);
+router.use("/students", studentRoutes);
+router.use("/subjects", subjectRoutes);
+router.use("/tutors", tutorRoutes);
 
 module.exports = router;
