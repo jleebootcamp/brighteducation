1bcae61387e74c81f6fec58ab193d62a56355cd0
Joon Lee <jlee@alphalab.com>
2022-10-13 18:50:37 -0700

    tutor routes

--- controllers/api/tutorRoutes.js
+++ controllers/api/tutorRoutes.js
@@ -1,3 +1,41 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Tutors } = require('../../models');
+
+// GET all tutors
+router.get('/', async (req, res) => {
+    try {
+      const tutorData = await Tutors.findAll({
+        include: [{ model: Tutors }],
+        attributes: {
+          include: [],
+        },
+      });
+      res.status(200).json(tutorData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // GET a single tutor
+  router.get('/:id', async (req, res) => {
+    try {
+      const driverData = await Tutors.findByPk(req.params.id, {
+        include: [{ model: Tutors }],
+        attributes: {
+          include: [],
+        },
+      });
+  
+      if (!tutorData) {
+        res.status(404).json({ message: 'No tutor found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(tutorData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
