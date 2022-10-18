b8826f9abedaa97d1ce1ae987423b609f89203ab
Csandu <ces.pnw@gmail.com>
2022-10-17 22:00:14 -0700

    route config

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ async function loginFormHandler(event) {
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           email,
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
       });
   
       if (response.ok) {
-        document.location.replace('homepage');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
