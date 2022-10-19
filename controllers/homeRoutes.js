21514f5105a3ee0950624c113a04a13c72ce1ebb
Joon Lee <jlee@alphalab.com>
2022-10-18 19:33:06 -0700

    fixed logins

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try{
-    res.render('homepage');
+    res.render('homepage', { logged_in: req.session.logged_in});
   } catch (err) {
     res.status(500).json(err);
   }
