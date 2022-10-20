b48a6f97674e24121b168525665ee3169e1f0b5d
Joon Lee <jlee@alphalab.com>
2022-10-20 10:01:49 -0700

    testing handlebar expression fetch/get

--- public/js/students.js
+++ public/js/students.js
@@ -1,1 +1,19 @@
-//testing
+//Populate tables
+
+const students = async () => {
+    const response = await fetch('/api/students/', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json'},
+    });
+  
+    if (response.ok) {
+      console.log('Fetched Students OK');
+      //console.log(JSON.stringify(response));
+      //document.location.replace('/my-student');
+    } else {
+      alert('No student records.');
+      console.log('fetch NOT OK');
+    }
+  };
+
+document.addEventListener('click', students);
