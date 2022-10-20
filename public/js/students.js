ec9bbabf37fc97fcc1621e729634e9976ff20ce2
Joon Lee <jlee@alphalab.com>
2022-10-20 12:50:49 -0700

    better console log

--- public/js/students.js
+++ public/js/students.js
@@ -4,16 +4,19 @@ const students = async () => {
     const response = await fetch('/api/students/', {
       method: 'GET',
       headers: { 'Content-Type': 'application/json'},
-    });
+    })
+    //.then(response => response.json())
   
     if (response.ok) {
       console.log('Fetched Students OK');
-      //console.log(JSON.stringify(response));
+      console.log(response.json());
+      console.log(response);
       //document.location.replace('/my-student');
+
     } else {
       alert('No student records.');
       console.log('fetch NOT OK');
     }
-  };
+};
 
 document.addEventListener('click', students);
